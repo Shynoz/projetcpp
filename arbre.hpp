5e687e038e10383dffe6e350dcf2ac2fbf45bf2d
Baptiste Devylder <55586844+Shynoz@users.noreply.github.com>
2020-11-20 17:31:32 +0100

    Update arbre.hpp

--- arbre.hpp
+++ arbre.hpp
@@ -7,16 +7,16 @@ class ArbreB {
 private:
   Sommet * root;
 protected:
-    Sommet* Insertion(Sommet* s, int val);
-    Sommet* Suppression(Sommet*s, int val);
-    Sommet* Fusion(Sommet* s1, Sommet * s2);
-    void print(Sommet * s);
-    Sommet* Recherche(Sommet *s,int val);
+    Sommet* Insertion(Sommet* s, int val); /* insertion d'un Sommet */
+    Sommet* Suppression(Sommet*s, int val); /* Supression d'un Sommet*/
+    Sommet* Fusion(Sommet* s1, Sommet * s2); /* Fusion de deux Sommet entre eux */
+    void print(Sommet * s);  /* affichage du Sommet */
+    Sommet* Recherche(Sommet *s,int val); /* Recherche du Sommet*/
 public:
 
   ArbreB();
 
-  void Insertion(int Valeur);
+  void Insertion(int Valeur);   
   void Suppression(int valeur);
   void print();
   bool Recherche(int Valeur);
